917efce43f4bd8a857d92adc9e38d731ebaf360e
Inturi <HI057133@cerner.net>
2018-01-25 16:43:37 -0600

    second commit

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
 int main() {
-	std::cout << "hello world";
-	system("pause");
+  std::cout << "hello world";
+  system("pause");
+  std::cout << "okay bye";
+  std::cout << "5";
+  std::cout << "zdsgdh";
 }
